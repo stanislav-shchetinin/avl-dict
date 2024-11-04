ca4661b104cca4a5cd3e732acd54ce97d5251ec1
Stas <stas.shc@gmail.com>
2024-11-05 02:21:58 +0300

    realized interface

--- src/avl_dict.ml
+++ src/avl_dict.ml
@@ -14,60 +14,95 @@ let empty = Empty
 
 let height = function
   | Empty -> 0
-  | Node { height; _ } -> height
-
-let create_node key value left right =
-  let height = 1 + max (height left) (height right) in
-  Node { key; value; left; right; height }
+  | Node {height; _} -> height
 
 let balance_factor = function
   | Empty -> 0
-  | Node { left; right; _ } -> height left - height right
+  | Node {left; right; _} -> height left - height right
 
-let rotate_right = function
-  | Node { key; value; left = Node { key = lk; value = lv; left = ll; right = lr; _ }; right; _ } ->
-      create_node lk lv ll (create_node key value lr right)
-  | node -> node
+let create_node key value left right = 
+  Node {key; value; left; right; height = 1 + max (height left) (height right) }
 
 let rotate_left = function
-  | Node { key; value; right = Node { key = rk; value = rv; left = rl; right = rr; _ }; left; _ } ->
-      create_node rk rv (create_node key value left rl) rr
+  | Node { key; value; left; right = Node { key = k1; value = v1; left = l1; right = r1; _}; _} ->
+    create_node k1 v1 (create_node key value left l1) r1
   | node -> node
 
-let balance node =
-  match node with
+let rotate_right = function
+  | Node { key; value; left = Node{ key = k1; value = v1; left = l1; right = r1; _}; right; _ } ->
+    create_node k1 v1 l1 (create_node key value r1 right)
+  | node -> node
+
+let balance = function
   | Empty -> Empty
-  | Node { key; value; left; right; _ } as n ->
-      let bf = balance_factor n in
-      if bf > 1 then
-        if balance_factor left >= 0 then rotate_right n
-        else create_node key value (rotate_left left) right |> rotate_right
-      else if bf < -1 then
-        if balance_factor right <= 0 then rotate_left n
-        else create_node key value left (rotate_right right) |> rotate_left
-      else n
+  | Node {key; value; left;  right; _} as node -> 
+    let bf = balance_factor node in
+    if bf = -2 then
+      if balance_factor right <= 0 then rotate_left node
+      else create_node key value left (rotate_right right) |> rotate_left
+    else if bf = 2 then
+      if balance_factor left <= 0 then rotate_right node
+      else create_node key value (rotate_left left) right |> rotate_right
+    else node
 
-let rec insert key value = function
+let find key = function
+  | Empty -> Empty
+  | Node {k = key; left; right; _} as node ->
+    if k < key then find key left
+    else if k > key find key right
+    else node
+
+let insert key value = function
   | Empty -> create_node key value Empty Empty
-  | Node { key = k; value = v; left; right; _ } ->
-      if key < k then
-        balance (create_node k v (insert key value left) right)
-      else if key > k then
-        balance (create_node k v left (insert key value right))
-      else
-        create_node key value left right
+  | Node {k = key; v = value; left; right; _} as node ->
+    if k < key then 
+      create_node k v (insert key value left) right |> balance
+    else if k > key 
+      create_node k v left (insert key value right) |> balance
+    else create_node key value left right
 
-let rec find key = function
-  | Empty -> None
-  | Node { key = k; value = v; left; right; _ } ->
-      if key < k then find key left
-      else if key > k then find key right
-      else Some v
+let min_node = function
+  | Empty -> Empty
+  | Node {left; _} as node ->
+    match left with
+      | Empty -> node
+      | _ -> min_node left
+
+let erase key = function
+  | Empty -> Empty
+  | Node {k = key; v = value; left; right; _} as node ->
+    if k < key then 
+      create_node k v (erase key value left) right |> balance
+    else if k > key 
+      create_node k v left (erase key value right) |> balance
+    else 
+      match node with
+      | Node {Empty; Empty; _} -> Empty
+      | Node {left; Empty; _} -> left
+      | Node {Empty; right; _} -> right
+      | Node {left; right; _} -> 
+        let er_node = min_node right in
+          match er_node with
+          | Node {k = key; v = value; l = left; r = right; _} ->
+            create_node k v left (erase k right)
 
 let is_empty = function
   | Empty -> true
   | _ -> false
 
-let rec to_list = function
+let to_list = function
   | Empty -> []
-  | Node { key; value; left; right; _ } -> to_list left @ [(key, value)] @ to_list right
+  | Node {key; value; left; right; _} -> to_list left @ [(key, value)] @ to_list right
+
+let filter (f: ('k, 'v) t -> bool) node =
+  if f node then
+    match node with
+    | Empty -> f Empty
+    | Node {key; value; left; right; _} -> create_node key value (f left) (f right)
+  else 
+    f (erase node)
+
+let map (f: 'v -> 'v) node = function
+  | Empty -> Empty
+  | Node {key; value; left; right; _} -> create_node key (f value) (map left) (map right) 
+
--- src/avl_dict.mli
+++ src/avl_dict.mli
@@ -3,7 +3,11 @@ type ('k, 'v) t
 
 val empty : ('k, 'v) t
 val insert : 'k -> 'v -> ('k, 'v) t -> ('k, 'v) t
-val find : 'k -> ('k, 'v) t -> 'v option
+val erase : 'k -> ('k, 'v) t -> ('k, 'v) t
+val find : 'k -> ('k, 'v) t -> ('k, 'v) t
 val height : ('k, 'v) t -> int
 val is_empty : ('k, 'v) t -> bool
 val to_list : ('k, 'v) t -> ('k * 'v) list
+val filter : (f: ('k, 'v) t -> bool) ('k, 'v) t -> ('k, 'v) t
+val map : (f: 'v -> 'v) ('k, 'v) t -> ('k, 'v) t
+
