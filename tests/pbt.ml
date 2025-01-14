b100117d3b45a470c11f26e2c61ffb3444c0e819
st-shchetinin <stas.shc@gmail.com>
2025-01-14 17:24:55 +0300

    pbt with qcheck

--- tests/pbt.ml
+++ tests/pbt.ml
@@ -1,32 +1,39 @@
 open Avl_dict
+open QCheck
 
-let () = Random.self_init ()
+(* Генератор случайных пар *)
+let pair_gen =
+  Gen.(map (fun (k, v) -> (k, v)) (pair (int_bound 100) (int_bound 100)))
 
-let random_pair () =
-  (Random.int 100, Random.int 100)
+(* Генератор списка пар *)
+let pair_list_gen =
+  Gen.list_size (Gen.int_bound 20) pair_gen
 
-let random_pair_list n =
-  List.init n (fun _ -> random_pair ())
+(* Генератор AVL-дерева *)
+let avl_tree_gen =
+  Gen.map
+    (fun pairs -> List.fold_left (fun tree (k, v) -> insert k v tree) empty pairs)
+    pair_list_gen
 
-let random_tree =
-  List.fold_left (fun tree (k, v) -> insert k v tree) empty (random_pair_list (Random.int 20))
+(* Преобразование генератора в arbitrary *)
+let avl_tree_arbitrary = make ~print:(fun _ -> "<avl_tree>") avl_tree_gen
 
-let check_neutral_element tree = 
-  equals tree (merge empty tree) && equals tree (merge tree empty)
+(* Тест нейтрального элемента *)
+let neutral_element_test =
+  Test.make
+    ~name:"Neutral element property"
+    avl_tree_arbitrary
+    (fun tree -> equals tree (merge empty tree) && equals tree (merge tree empty))
 
-let neutral_element_test = 
-  assert (check_neutral_element random_tree)
+(* Тест ассоциативности *)
+let associativity_test =
+  Test.make
+    ~name:"Associativity property"
+    Gen.(triple avl_tree_gen avl_tree_gen avl_tree_gen |> make)
+    (fun (a, b, c) ->
+      equals (merge a (merge b c)) (merge (merge a b) c))
 
-let associativity_test = 
-  let a = random_tree in
-  let b = random_tree in
-  let c = random_tree in
-  assert (
-    equals (merge a (merge b c)) (merge (merge a b) c)
-  )
-
-(* Monoid tests *)
-let () = neutral_element_test
-let () = associativity_test
-
-let() = Printf.printf "Monoid tests: OK\n"
+(* Запуск тестов *)
+let () =
+  QCheck_base_runner.run_tests_main
+    [neutral_element_test; associativity_test]
